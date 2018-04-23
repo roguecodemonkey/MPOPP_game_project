6103788054a4f829b7faf3918acb5f2005850160
grantclarke <g.clarke@abertay.ac.uk>
2018-04-23 20:39:13 +0800

    second change from grantclarke-abertay

--- Game/Assets/player.cs
+++ Game/Assets/player.cs
@@ -11,6 +11,6 @@ public class player : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-        print("conflict resolved from roguecodemonkey");
+        print("conflict resolved from grantclarke-abertay");
 	}
 }
